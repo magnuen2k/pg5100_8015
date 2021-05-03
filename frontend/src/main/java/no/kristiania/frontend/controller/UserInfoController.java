2a2ef40dc6e17e7a51a6bd8c1ce87f5c9cb3eb33
magnuen2k <maen030@student.kristiania.no>
2021-05-03 12:50:35 +0200

    add basic frontend

---
+++ frontend/src/main/java/no/kristiania/frontend/controller/UserInfoController.java
@@ -0,0 +1,21 @@
+package no.kristiania.frontend.controller;
+
+import no.kristiania.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class UserInfoController {
+
+    @Autowired
+    private UserService userService;
+
+    public String getUserName(){
+        return ((UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername();
+    }
+}
---
+++ frontend/src/test/java/no/kristiania/LocalApplicationRunner.java
@@ -0,0 +1,10 @@
+package no.kristiania;
+
+import org.springframework.boot.SpringApplication;
+
+public class LocalApplicationRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, "--spring.profiles.active=test");
+    }
+}
+
